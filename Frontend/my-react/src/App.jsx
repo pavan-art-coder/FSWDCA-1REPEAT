215836db369e4266b626f1948c8671b64e0b94c3
PAVEN NJADHAV <pavannjadhav12@gmail.com>
2025-04-17 11:31:00 +0530

    Done

---
+++ Frontend/my-react/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ServiceCard from './components/ServiceCard'
+
+function App() {
+
+  return (
+    <>
+      <ServiceCard title={"Web Development"} description={"Html,Css,Javascript are used to build this"}/>
+      <ServiceCard title={"Applications of Web Development"} description={"From this we can Build Ecommerce Websites"}/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/my-react/src/components/ServiceCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ServiceCard({title,description}) {
+  return (
+    <>
+    <h1>Service Card</h1>
+    <h2>Title:{title}</h2>
+    <p>Description:{description}</p>
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default ServiceCard
